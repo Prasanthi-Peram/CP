162227c86663c51263f738ec364cc82d416f4d5f
Prasanthi-Peram <prasanthiperam29@gmail.com>
2024-05-30 10:17:45 +0530

    Tree updated

---
+++ CSES/Introductory/repetitions.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*int main(){
+    string s;
+    cin>>s;
+
+    map<char,int> count;
+    for(int i=0;i<s.length();i++){
+        count[s[i]]++;
+    }
+    map<char,int> :: iterator it;
+    it=count.begin();
+    int max= it->second;
+    for(it=count.begin();it!=count.end();it++){
+        if(it->second>max) max=it->second;
+    }
+    cout<<max<<endl;
+    return 0;
+}
+This code is for mode
+*/
+
+/*int main(){
+    string s;
+    cin>>s;
+
+    int max1,max2;
+    max1=max2=0;
+    char a,b;
+    a=s[0];
+
+    for(int i=0;i<s.length();i++){
+        if(s[i]==a)
+    }
+    return 0;
+}*/
