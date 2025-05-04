e783ee2948d75778a5c9a30a2f2ada24ec09b8b9
Peram Prasanthi <142584090+Prasanthi-Peram@users.noreply.github.com>
2025-05-04 17:25:41 +0530

    I'll complete it later

--- CSES/Labyrinth.cpp
+++ CSES/Labyrinth.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<string> a;
+using state = pair<int,int>;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+//This problem had graph and a different application of BFS: Implicit graph
 
+int main(){
+  int n,m;
+  cin>>n>>m;
+  a.resize(n);
+  for(int i=0;i<n;i++) cin>>a[i];
+
+  state st,en;
+  rep(i,0,n){
+    
+  }
+  
+  return 0;
+}
